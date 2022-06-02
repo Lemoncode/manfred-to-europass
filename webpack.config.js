28cdd040dbe8e5780f7e99497544a98482a19ab0
Félix Franco <felix.franco.rojas@gmail.com>
2022-06-02 12:44:42 +0200

    add tsconfig file, webpack file and initials file

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const basePath = __dirname;
+
+module.exports = {
+    context: path.join(basePath, "src"),
+    resolve: {
+        extensions: [".js", ".ts", ".tsx"],
+    },
+    entry: {
+        app: ["./index.tsx"],
+    },
+    devtool: "eval-source-map",
+    stats: "errors-only",
+    output: {
+        filename: "[name].[chunkhash].js",
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.html$/,
+                loader: "html-loader",
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "index.html",
+        }),
+    ],
+};
