45acc5a33087a6389098a0084e12a0401072da3b
Braulio Diez Botella <braulio.diez@gmail.com>
2022-06-09 21:34:38 +0200

    engine

---
+++ src/engine/index.ts
@@ -0,0 +1,17 @@
+// TODO add Aliases
+import * as model from "../models/europass";
+
+export const generateSkillsPassport = (
+  skillsPassport: model.SkillsPassport
+) => {
+  const output: string = "";
+
+  /*
+  // TODO Add XMLWriter like parameter
+  generateDocumentInfo();
+  generatePrintingPreferences();
+  generateLearnerInfo();
+  */
+
+  return output;
+};
