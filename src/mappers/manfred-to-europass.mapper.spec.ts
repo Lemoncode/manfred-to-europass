37e5a8f9a2beb82f52b026f0aa5d8e572c23098b
ortiztena <ortiztena@gmail.com>
2022-06-21 10:02:51 +0200

    refactor files. added /schema to gitignore

--- src/mappers/manfred-to-europass.mapper.spec.ts
+++ src/mappers/manfred-to-europass.mapper.spec.ts
@@ -3,16 +3,15 @@ import { mapFromModelToEuropass } from "@/mappers/manfred-to-europass.mapper";
 import * as baseModel from '@/models/manfred'
 import * as finalModel from '@/models/europass'
 
-
-
 describe("mapper.tests", () => {
   it(`should return empty object when it feeds a cv which equals to undefined
   `, () => {
+
     // Arrange
-    const dataCvManfred: baseModel.ManfredAwesomicCV = undefined
+    const dummyCv: baseModel.ManfredAwesomicCV = undefined
 
     // Act
-    const result: finalModel.SkillsPassport = mapFromModelToEuropass(dataCvManfred);
+    const result: finalModel.SkillsPassport = mapFromModelToEuropass(dummyCv);
 
     // Assert
     expect(result).toStrictEqual({})
@@ -20,29 +19,28 @@ describe("mapper.tests", () => {
 
   it(`should return empty object when it feeds a cv which equals to null
   `, () => {
+
     // Arrange
-    const dataCvManfred: baseModel.ManfredAwesomicCV = null
+    const dummyCv: baseModel.ManfredAwesomicCV = null
 
     // Act
-    const result: finalModel.SkillsPassport = mapFromModelToEuropass(dataCvManfred);
+    const result: finalModel.SkillsPassport = mapFromModelToEuropass(dummyCv);
 
     // Assert
     expect(result).toStrictEqual({})
   });
 
-
   it(`When it feeds a string name and surnames, it returns same string on FirstName and SurName
   `, () => {
-    // Arrange
 
+    // Arrange
     const dummyCv: baseModel.ManfredAwesomicCV = {
       settings: { language: 'english' },
       aboutMe: { profile: { name: "Manfred", surnames: "Awesomic", title: 'Sr' } }
     };
-    const result: finalModel.SkillsPassport = mapFromModelToEuropass(dummyCv);
 
     // Act
-
+    const result: finalModel.SkillsPassport = mapFromModelToEuropass(dummyCv);
     const path = result.learnerInfo.identification;
 
     // Assert
@@ -57,14 +55,15 @@ describe("mapper.tests", () => {
       settings: { language: null },
       aboutMe: { profile: { name: null, surnames: null, title: null } }
     };
-    // Act
 
+    // Act
     const result: finalModel.SkillsPassport = mapFromModelToEuropass(dummyCv);
     const path = result.learnerInfo.identification;
-    // Assert
 
+    // Assert
     expect(path.personName.firstName).toBe(null);
     expect(path.personName.surName).toBe(null);
+
   });
 });
 
