d1c64665a378af95bc95fa06db8e554f7d63a3a6
Andrés Rojas <eurekacloud2019@gmail.com>
2022-07-06 12:09:45 +0200

    Remove useless styles

--- src/common/components/card/card.component.styles.ts
+++ src/common/components/card/card.component.styles.ts
@@ -5,7 +5,6 @@ export const main = css`
   position: absolute;
   top: 15vh;
   flex-direction: column;
-  justify-content: space-around;
   align-items: center;
   padding: 50px 0;
   gap: 50px;
@@ -22,17 +21,4 @@ export const title = css`
   line-height: 23px;
   padding: 0;
   margin: 0;
-`;
-
-export const beginButton = css`
-  width: 166px;
-  height: 40px;
-  font-style: normal;
-  font-weight: 700;
-  font-size: 24px;
-  line-height: 23px;
-  background: #39d65c;
-  border: none;
-  border-radius: 20px;
-  color: #ffffff;
 `;
