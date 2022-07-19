57f9bd9daf5db3ab1c18214945e01abcc5777fbe
Andrés Rojas <eurekacloud2019@gmail.com>
2022-07-19 13:30:14 +0200

    Update material ui imports

--- src/core/theme/theme.hooks.ts
+++ src/core/theme/theme.hooks.ts
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useTheme } from '@mui/material'
+import { useTheme } from '@material-ui/core'
 
 export type ResultType<Classes> = {
   [K in keyof Classes]?: Classes[K] extends string | Function
